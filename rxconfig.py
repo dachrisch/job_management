81ca73b62f60fd49124193788388e131559bf020
dachrisch <christian.daehn@gmail.com>
2024-09-10 12:43:41 +0200

    Update default API URL and add print statement

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,9 @@ import os
 
 import reflex as rx
 
+api_url = os.getenv('JOB_API_URL', 'http://localhost:8000')
+print(f'Running with backend at: {api_url}')
 config = rx.Config(
     app_name="job_management",
-    api_url=os.getenv('JOB_API_URL', 'http://localhost:8000'),
+    api_url=api_url,
 )

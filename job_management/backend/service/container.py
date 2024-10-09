3768c637385d45685995c2e1ed6f467f1d7f7f75
dachrisch <christian.daehn@gmail.com>
2024-10-09 13:55:55 +0200

    Add GoogleCredentialsService import to container.py

--- job_management/backend/service/container.py
+++ job_management/backend/service/container.py
@@ -7,11 +7,11 @@ from scrapy.utils.project import get_project_settings
 from job_management.backend import state, service
 from job_management.backend.service.application import JobApplicationService
 from job_management.backend.service.cv import CvService
+from job_management.backend.service.google import GoogleCredentialsService
 from job_management.backend.service.job_offer import JobOfferService
 from job_management.backend.service.site import JobSitesService
 from job_management.backend.service.sites_with_jobs import JobSitesWithJobsService
 from job_management.backend.service.storage import JobApplicationStorageService
-from job_management.backend.service.google import GoogleCredentialsService
 from job_offer_spider.db.job_management import MontyJobManagementDb, MongoJobManagementDb
 
 

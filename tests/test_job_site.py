52652bcba09edf2b97fa967c571aeaf607b9e9c3
dachrisch <christian.daehn@gmail.com>
2024-10-18 14:37:35 +0200

    Fix spacing and alignment issues in test_job_site.py

--- tests/test_job_site.py
+++ tests/test_job_site.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from unittest import TestCase
 
-
 from job_management.backend.entity.site import JobSite
 from job_management.backend.entity.stat import Statistics
 from job_offer_spider.item.db.sites import JobSiteDto, JobStatistic
@@ -9,7 +8,7 @@ from job_offer_spider.item.db.sites import JobSiteDto, JobStatistic
 
 class JobOfferTest(TestCase):
     def setUp(self):
-        self.now=datetime.now()
+        self.now = datetime.now()
 
     def test_jobs_dto(self):
         dto_from_dict = JobSiteDto.from_dict({'url': 'test', 'jobs': {'total': 1}})
@@ -18,12 +17,12 @@ class JobOfferTest(TestCase):
 
     def test_jobs_frontend(self):
         job_site_parse_obj = JobSite.parse_obj({'title': 'test', 'jobs': {'total': 1}})
-        self.assertEqual(JobSite(title='test', jobs=Statistics(total=1),added=job_site_parse_obj.added),
+        self.assertEqual(JobSite(title='test', jobs=Statistics(total=1), added=job_site_parse_obj.added),
                          job_site_parse_obj)
 
     def test_jobs_frontend_none(self):
         job_site = JobSite(**{'title': 'test', 'jobs': None, 'added': self.now.toordinal()})
-        self.assertEqual(JobSite(title='test', jobs=Statistics(total=0),added=job_site.added),
+        self.assertEqual(JobSite(title='test', jobs=Statistics(total=0), added=job_site.added),
                          job_site)
 
     def test_jobs_frontend_none_dict(self):
